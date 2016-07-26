0e8f0481e1c71f96602d18e312dfe9a94bf37283
Peter Healy <peterhealy@Peters-MacBook.local>
2016-07-26 23:15:11 +0100

    adding swagger info tocontroller

--- src/main/java/com/phealy1982/controllers/PlayerController.java
+++ src/main/java/com/phealy1982/controllers/PlayerController.java
@@ -2,25 +2,31 @@ package com.phealy1982.controllers;
 
 import com.phealy1982.models.Player;
 import com.phealy1982.util.URIConstant;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api( value = "/player", description = "Manage players" )
 @RestController
 public class PlayerController {
 
+    @ApiOperation(
+            value = "Addd a player",
+            notes = "Add information for a new player"
+    )
     @RequestMapping(value = URIConstant.PLAYER_ADD_URI, method = RequestMethod.POST)
     void add() {
 
-
     }
 
+    @ApiOperation(
+            value = "Get a player",
+            notes = "Get information for specified player",
+            response = Player.class
+    )
     @RequestMapping(value = URIConstant.PLAYER_GET_URI, method = RequestMethod.GET)
     Player get(@PathVariable(value="id")  String id){
-
-        Player resp = new Player();
-        resp.setName("Dummy");
-
-        return resp;
-
+        return new Player("Dummy");
     }
 
 
