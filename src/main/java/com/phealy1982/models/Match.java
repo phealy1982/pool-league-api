04f1522be07afa574993841ba0c66584119a51db
phealy1982 <phealy1982@gmail.com>
2017-07-02 18:08:15 +0100

    changes

--- src/main/java/com/phealy1982/models/Match.java
+++ src/main/java/com/phealy1982/models/Match.java
@@ -18,14 +18,22 @@ public class Match {
     private String playerTwoScore;
     private String league;
     private String week;
+    private String division;
+    private String season;
+    private String time;
+    private String table;
 
-    public Match(String playerOne, String playerOneScore, String playerTwo, String playerTwoScore, String league, String week) {
+    public Match(String playerOne, String playerOneScore, String playerTwo, String playerTwoScore, String league, String week, String division, String season, String time, String table) {
         this.playerOne = playerOne;
         this.playerOneScore = playerOneScore;
         this.playerTwo = playerTwo;
         this.playerTwoScore = playerTwoScore;
         this.league = league;
         this.week = week;
+        this.division = division;
+        this.season = season;
+        this.time = time;
+        this.table = table;
     }
 
     public String getPlayerOne() {
@@ -74,5 +82,37 @@ public class Match {
 
     public void setWeek(String week) {
         this.week = week;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
     }
 }
